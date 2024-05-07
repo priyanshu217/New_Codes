2a46a0ce3264fd2e6fe57a2521033c934ad379cc
priyanshu217 <priyanshusaxena20@gmail.com>
2024-05-07 07:11:36 +0530

    well done

---
+++ c - pro/Volbo.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+
+    // int r = 7;
+    // float v = 4 * 3.14 * r * r * r / 3;
+    // printf("The Volume is : %f", v);
+
+
+    int r = 8;
+
+    float v = 3.14 * r * r ;
+    int c = 3.14 * r * r ;
+    printf("Here it is: %f", v);       // for float value
+
+    printf("\n");                        
+    printf("Here it is: %f", c);       // for integer value
+
+    return 0;
+}
---
+++ c - pro/second.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+
+    // int x = 5;                      %d ------->  Integer value
+    // int y = 2;                      %f ------->  Float value
+
+    // printf("%d\n", x+y);
+    // printf("%d\n", x-y);
+    // printf("%d\n", x*y);
+    // printf("%d\n", x/y);
+
+
+
+
+    return 0;
+}
