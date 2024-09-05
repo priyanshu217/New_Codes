bfed9a1643f867f6336065eace73080d08ff24c3
Priyanshu <129535434+priyanshu217@users.noreply.github.com>
2024-09-05 17:37:29 +0530

    hello

---
+++ new_c++_lecs/c_in_and_out.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std; 
+
+int main (){
+    int a,b,c;
+    cout<<"Enter First Number";
+    cin>>a;
+    cout<<"Enter Second Number";
+    cin>>b;
+    c=a+b;
+    cout<<"Addition = "<<c;
+
+    cout<<"\nMy Self Priyanshu\n heloo";
+
+    return 0;
+}
