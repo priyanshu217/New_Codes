915784302852475a05950d1f567a89e31691170f
Priyanshu <129535434+priyanshu217@users.noreply.github.com>
2024-10-06 14:29:16 +0530

    new

--- new_c++_lecs/Lec13.cpp
+++ new_c++_lecs/Lec13.cpp
@@ -16,16 +16,31 @@ int main(){
       
     //   HERE ARE THE PROGRAM TO PRINT THE Nth NUMBER
 
-    int i , n ;
-    i = 1 ;
-    cout<<"\nEnter the nth Number of Your Choice : ";
+    // int i , n ;
+    // i = 1 ;
+    // cout<<"\nEnter the nth Number of Your Choice : ";
+    // cin>>n;
+
+    // while (i<=n)
+    // {
+    //     cout << i << "\n";
+    //     i = i+1;
+    // }
+
+    // PROGRAM TO PRINT ALL EVEN NUMBERS
+
+    int i = 2 , n;
+    cout<< "\n Enter Your Desire number here : " ;
     cin>>n;
+
 
     while (i<=n)
     {
-        cout << i << "\n";
-        i = i+1;
+        cout<<i<<"\n";
+        i = i+2;
+
     }
+    
     
 
     
